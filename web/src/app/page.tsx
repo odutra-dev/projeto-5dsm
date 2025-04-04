cdc9acf2c8fe72b5ac7d2c256e981bbe465024e1
DutraGames <gabrieldutraamarante@gmail.com>
2025-04-04 11:57:38 -0300

    refactor(web): criado o header da pagina inicial

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <header className="bg-primary h-32 relative">
+        <Image
+          className="absolute left-1/2 top-1/2 -translate-x-1/2"
+          src="/logo.svg"
+          alt="Geladinho Santista"
+          width={180}
+          height={180}
+        />
+      </header>
+
+      <main className="container">
+        
+      </main>
+    </>
+  );
 }
