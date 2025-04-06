19f4928eb2cf0921ec832efeaeaa7c03b4581f9c
lohan.parrilla@gmail.com <lohan.parrilla@gmail.com>
2025-04-06 20:08:14 -0300

    refactor(back-end): Adicionado tipagens para os produtos
    Co-authored-by: DutraGames <Gabrieldutraamarante@gmail.com>

---
+++ back-end/src/@types/typesProdutos.ts
@@ -0,0 +1,12 @@
+export type Produto = {
+    id: string;
+    nome: string;
+    descricao: string;
+    preco: number;
+  };
+
+  export type NovoProduto = {
+    nome: string;
+    descricao: string;
+    preco: number;
+  };
