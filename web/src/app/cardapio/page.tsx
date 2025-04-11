2c92177c923cea21af08e528bdfea55940b50c0e
DutraGames <gabrieldutraamarante@gmail.com>
2025-04-11 20:56:27 -0300

    refactor(web): Adiciona max-width

--- web/src/app/cardapio/page.tsx
+++ web/src/app/cardapio/page.tsx
@@ -111,7 +111,7 @@ export const Cardapio = () => {
           </div>
         </section>
 
-        <section className="mt-8 flex flex-col gap-4">
+        <section className="mt-8 flex flex-col gap-4 w-full md:max-w-xl">
           {produtos.map((produto) => (
             <Card
               id={produto.id}
