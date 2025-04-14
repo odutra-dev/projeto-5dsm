19c8f0d595a8eef01c06e73ebfc1e33f2409cde8
DutraGames <gabrieldutraamarante@gmail.com>
2025-04-13 21:32:36 -0300

    fix(web): corrigido o domínio do storage

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "storage.googleapis.com",
+        hostname: "firebasestorage.googleapis.com",
       },
     ],
   },
