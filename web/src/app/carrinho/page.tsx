5429ae15f5b637b661869a6cd26483556d2b55fd
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-22 13:03:09 -0300

    refactor(web): adiciona seleção dos metodos de pagamento e entrega

--- web/src/app/carrinho/page.tsx
+++ web/src/app/carrinho/page.tsx
@@ -2,11 +2,19 @@
 import { CardCarrinho } from "@/components/CardCarrinho";
 import Header from "@/components/Header";
 import Image from "next/image";
-
 import { useCarrinho } from "@/context/carrinho";
+import { useState } from "react";
 
 export const Carrinho = () => {
   const { carrinho } = useCarrinho();
+
+  const [metodoPagamento, setMetodoPagamento] = useState<string | null>(
+    "Dinheiro"
+  );
+  const [metodoEntrega, setMetodoEntrega] = useState<string | null>("Delivery");
+
+  const metodosPagamento = ["Dinheiro", "Débito", "Crédito", "Pix"];
+  const metodosEntrega = ["Delivery", "Retirada"];
 
   return (
     <>
@@ -14,11 +22,11 @@ export const Carrinho = () => {
         icon="back"
         title="Carrinho"
         icon2="shop"
-        link="/"
+        link="/cardapio"
         link2="/cardapio"
       />
 
-      <main className=" flex justify-center flex-col items-center px-6 w-full">
+      <main className="flex justify-center flex-col items-center px-6 w-full">
         {carrinho.map((item) => (
           <CardCarrinho
             key={item.id}
@@ -35,62 +43,90 @@ export const Carrinho = () => {
             Resumo do Pedido
           </h2>
           <div className="text-primary-text flex justify-between">
-            <p className="">Subtotal:</p>
-            <p className=""> R$ 11,00</p>
+            <p>Subtotal:</p>
+            <p>
+              R$
+              {carrinho
+                .reduce(
+                  (acc, item) => acc + item.preco * (item.quantidade || 1),
+                  0
+                )
+                .toFixed(2)}
+            </p>
           </div>
           <div className="text-primary-text flex justify-between">
-            <p className="">Taxa de entrega:</p>
-            <p className=""> R$ 5,00</p>
+            <p>Taxa de entrega:</p>
+            <p>{metodoEntrega === "Delivery" ? "R$ 5,00" : "R$ 0,00"}</p>
           </div>
           <hr className="w-full border-1 border-primary border-dashed" />
           <div className="text-primary-text font-bold flex justify-between">
-            <p className="">Total:</p>
-            <p className=""> R$ 16,00</p>
+            <p>Total:</p>
+            <p>
+              R${" "}
+              {carrinho
+                .reduce(
+                  (acc, item) => acc + item.preco * (item.quantidade || 1),
+                  0
+                )
+                .toFixed(2)}
+            </p>
           </div>
         </div>
 
+        {/* Método de Pagamento */}
         <div className="mt-6 w-full md:max-w-xl">
           <h2 className="text-center font-bold text-primary-text text-2xl">
             Método de Pagamento
           </h2>
-          <div className="w-full md:max-w-xl flex rounded-2xl justify-center  p-4 gap-4">
-            <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-              <p>Dinheiro</p>
-            </div>
-            <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-              <p>Débito</p>
-            </div>
-            <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-              <p>Crédito</p>
-            </div>
-            <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-              <p>Pix</p>
-            </div>
+          <div className="w-full md:max-w-xl flex flex-wrap justify-center p-4 gap-4">
+            {metodosPagamento.map((metodo) => (
+              <div
+                key={metodo}
+                onClick={() => setMetodoPagamento(metodo)}
+                className={`p-4 border-1 border-primary-text rounded-2xl cursor-pointer
+                  ${
+                    metodoPagamento === metodo
+                      ? "bg-primary-text text-secondary"
+                      : "text-primary-text"
+                  }`}
+              >
+                <p>{metodo}</p>
+              </div>
+            ))}
           </div>
         </div>
 
+        {/* Método de Entrega */}
         <div className="my-6 w-full md:max-w-xl flex flex-col gap-4">
           <h2 className="text-center font-bold text-primary-text text-2xl">
             Método de Entrega
           </h2>
 
-          <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-            <p>Delivery</p>
-          </div>
-          <div className="p-4 border-1 border-primary-text rounded-2xl text-primary-text">
-            <p>Retiada</p>
-          </div>
+          {metodosEntrega.map((metodo) => (
+            <div
+              key={metodo}
+              onClick={() => setMetodoEntrega(metodo)}
+              className={`p-4 border-2 border-primary-text rounded-2xl cursor-pointer
+                ${
+                  metodoEntrega === metodo
+                    ? "bg-primary-foreground text-secondary"
+                    : "text-primary-text"
+                }`}
+            >
+              <p>{metodo}</p>
+            </div>
+          ))}
         </div>
 
-        <button className="flex justify-center items-center gap-4 mb-6 w-full md:max-w-xl h-12 md:h-16 bg-primary border-2 border-primary-foreground rounded-2xl text-secondary font-bold text-xl md:text-2xl">
+        <a className="flex justify-center items-center gap-4 mb-6 w-full md:max-w-xl h-12 md:h-16 bg-primary border-2 border-primary-foreground rounded-2xl text-secondary font-bold text-xl md:text-2xl">
           Finalizar pedido
           <Image
-            src="/back.svg"
+            src="/seta.svg"
             alt="seta que aponta para a direita"
             width={16}
             height={16}
           />
-        </button>
+        </a>
       </main>
     </>
   );
