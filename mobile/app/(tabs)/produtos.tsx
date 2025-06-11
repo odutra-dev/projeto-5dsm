b090a3a2a7b439930887cc1c900dd0aa70e64934
DutraGames <gabrieldutraamarante@gmail.com>
2025-06-11 18:20:34 -0300

    refactor(mobile): cria a página de produtos

---
+++ mobile/app/(tabs)/produtos.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import Header from "../../components/Header";
+
+export default function Produtos() {
+  return (
+    <View>
+      <Header titulo="Produtos" />
+    </View>
+  );
+}
