f92fc5686e56ca0c1d8d5237549219a5b8adf4ed
lohan.parrilla@gmail.com <lohan.parrilla@gmail.com>
2025-06-12 20:07:31 -0300

    feat(back-end): Adicionado PUT para mudar o valor dentro do Atributo Status no Pedido.

--- back-end/src/repositories/PedidoRepository.ts
+++ back-end/src/repositories/PedidoRepository.ts
@@ -1,24 +1,25 @@
 import { collection, doc, setDoc, getDocs, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import { NovoPedido, Pedido } from "../@types/typesPedidos";
+
 export class PedidoRepository {
   async create(pedido: NovoPedido): Promise<Pedido> {
     const novaRef = doc(collection(db, "pedidos"));
     const idGerado = novaRef.id;
-  
+
     if (
       !pedido.data ||
       !pedido.horario ||
       !pedido.tipo_entrega ||
       !pedido.tipo_pagamento ||
       !pedido.clienteId ||
-      !pedido.produtos  ||
+      !pedido.produtos ||
       !pedido.status ||
       !pedido.valor
     ) {
       throw new Error("Campos obrigatórios do pedido ausentes ou inválidos.");
     }
-  
+
     const novoPedido: Pedido = {
       id: idGerado,
       data: pedido.data,
@@ -30,37 +31,40 @@ export class PedidoRepository {
       status: pedido.status,
       valor: pedido.valor,
     };
-  
+
     const pedidoLimpo = JSON.parse(JSON.stringify(novoPedido));
     await setDoc(novaRef, pedidoLimpo);
     return novoPedido;
   }
-  
-  
 
-    async findAll(): Promise<Pedido[]> {
-        const pedidoCol = collection(db, "pedidos");
-        const snapshot = await getDocs(pedidoCol);
-    
-        const pedidos: Pedido[] = snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...(doc.data() as Omit<Pedido, "id">)
-        }));
-    
-        return pedidos;
-      }
-    
-      async findById(id: string): Promise<Pedido | null> {
-        const docRef = doc(db, "pedidos", id);
-        const snap = await getDoc(docRef);
-    
-        if (snap.exists()) {
-          return {
-            id: snap.id,
-            ...(snap.data() as Omit<Pedido, "id">)
-          };
-        }
-        
-        return null;
-      }
+  async findAll(): Promise<Pedido[]> {
+    const pedidoCol = collection(db, "pedidos");
+    const snapshot = await getDocs(pedidoCol);
+
+    const pedidos: Pedido[] = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...(doc.data() as Omit<Pedido, "id">)
+    }));
+
+    return pedidos;
+  }
+
+  async findById(id: string): Promise<Pedido | null> {
+    const docRef = doc(db, "pedidos", id);
+    const snap = await getDoc(docRef);
+
+    if (snap.exists()) {
+      return {
+        id: snap.id,
+        ...(snap.data() as Omit<Pedido, "id">)
+      };
     }
+
+    return null;
+  }
+
+  async updateStatus(id: string, status: string): Promise<void> {
+    const pedidoRef = doc(db, "pedidos", id);
+    await updateDoc(pedidoRef, { status });
+  }
+}
