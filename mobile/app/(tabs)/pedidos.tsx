1a2e609a1c2053bbe9ef9baa579cc40a0bad9751
DutraGames <gabrieldutraamarante@gmail.com>
2025-06-11 23:00:51 -0300

    refactor(mobile): cria cards dos pedidos

--- mobile/app/(tabs)/pedidos.tsx
+++ mobile/app/(tabs)/pedidos.tsx
@@ -1,9 +1,71 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import Header from "../../components/Header";
+import theme from "../../theme";
+import { useState } from "react";
+import CardPedido from "../../components/CardPedido";
 
 export default function Pedidos() {
+  const [status, setStatus] = useState("PENDENTE");
+
+  const [pedidos, setPedidos] = useState([
+    {
+      numero: "ORD-006",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "PENDENTE",
+    },
+    {
+      numero: "ORD-006",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "EMPRODUCAO",
+    },
+    {
+      numero: "ORD-006",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "PRONTO",
+    },
+    {
+      numero: "ORD-006",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "CONCLUIDO",
+    },
+  ]);
+
   return (
-    <View>
-      <Text>Pedidos</Text>
+    <View style={styles.container}>
+      <Header titulo="Pedidos" />
+
+      <FlatList
+        data={pedidos}
+        keyExtractor={(item) => item.numero}
+        renderItem={({ item }) => <CardPedido {...item} />}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.yellowBG[100],
+  },
+});
