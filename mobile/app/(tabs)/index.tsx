49e98670168d003b44af7620281e3c44c91763ef
DutraGames <gabrieldutraamarante@gmail.com>
2025-06-12 21:25:18 -0300

    refactor(mobile): Adiciona pedidos template no dashboard

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,15 +1,66 @@
-import { View, Text, StyleSheet, FlatList, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Header from "../../components/Header";
 import theme from "../../theme";
 import CardStatus from "../../components/CardStatus";
+import CardPedido from "../../components/CardPedido";
 
 export default function Home() {
   const [user, setUser] = useState("");
 
   const router = useRouter();
+
+  const pedidos = [
+    {
+      numero: "ORD-006",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "PENDENTE",
+    },
+    {
+      numero: "ORD-007",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "EMPRODUCAO",
+    },
+    {
+      numero: "ORD-008",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "PRONTO",
+    },
+    {
+      numero: "ORD-009",
+      nome: "Eduardo Lima",
+      endereco: "Rua dos Ipês, 987 - Alto da Boa Vista",
+      horario: "11:45",
+      itens: 21,
+      pagamento: "Cartão de Crédito",
+      valor: "R$ 120,00",
+      status: "CONCLUIDO",
+    },
+  ];
 
   useEffect(() => {
     const getUser = async () => {
@@ -26,43 +77,37 @@ export default function Home() {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <Header titulo="Dashboard" />
-      <View style={styles.main}>
-        <Text style={styles.titulo}>Olá, {user}!</Text>
+      <FlatList
+        contentContainerStyle={styles.flatListContainer}
+        ListHeaderComponent={
+          <>
+            <Text style={styles.titulo}>Olá, {user}!</Text>
 
-        <Text style={[styles.subtitulo, { marginTop: 32 }]}>Resumo Geral</Text>
-        <View style={styles.cards}>
-          <CardStatus numero={4} status="PENDENTE" />
-          <CardStatus numero={2} status="EMPRODUCAO" />
-          <CardStatus numero={1} status="PRONTO" />
-          <CardStatus numero={3} status="CONCLUIDO" />
-        </View>
-
-        <Text style={[styles.subtitulo, { marginTop: 32 }]}>
-          Pedidos Recentes
-        </Text>
-
-        <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-          keyExtractor={(item) => String(item)}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                backgroundColor: theme.colors.rosePrincipal[100],
-                padding: 16,
-                borderRadius: 8,
-                marginBottom: 16,
-              }}
-            >
-              <Text style={{ color: theme.colors.rosePrincipal[500] }}>
-                {item}
-              </Text>
+            <Text style={[styles.subtitulo, { marginTop: 32 }]}>
+              Resumo Geral
+            </Text>
+            <View style={styles.cards}>
+              <CardStatus numero={4} status="PENDENTE" />
+              <CardStatus numero={2} status="EMPRODUCAO" />
+              <CardStatus numero={1} status="PRONTO" />
+              <CardStatus numero={3} status="CONCLUIDO" />
             </View>
-          )}
-        />
-      </View>
-    </ScrollView>
+
+            <View style={styles.main}>
+              <Text style={[styles.subtitulo]}>Pedidos Recentes</Text>
+              <TouchableOpacity>
+                <Text style={styles.verTodos}>Ver todos</Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        }
+        data={pedidos}
+        keyExtractor={(item) => item.numero}
+        renderItem={({ item }) => <CardPedido {...item} />}
+      />
+    </View>
   );
 }
 
@@ -72,8 +117,9 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.yellowBG[100],
   },
   main: {
-    paddingHorizontal: 24,
-    marginTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 32,
   },
   titulo: {
     color: theme.colors.rosePrincipal[500],
@@ -92,5 +138,15 @@ const styles = StyleSheet.create({
     gap: 16,
     flexWrap: "wrap",
     width: "100%",
+  },
+  flatListContainer: {
+    paddingHorizontal: 24,
+    paddingVertical: 32,
+  },
+
+  verTodos: {
+    color: theme.colors.rosePrincipal[500],
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
